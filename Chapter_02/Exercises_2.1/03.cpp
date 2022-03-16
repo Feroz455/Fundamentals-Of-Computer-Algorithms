fa953805440a1338187878e58a4c00965703b8dd
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-03-16 08:50:48 +0600

    Update 03.cpp

--- Chapter_02/Exercises_2.1/03.cpp
+++ Chapter_02/Exercises_2.1/03.cpp
@@ -5,43 +5,46 @@ Write algorithm for AddQ and deleteQ, assuming the queue is represent as a link 
 
 #include <bits/stdc++.h>
 using namespace std;
- 
-struct QNode {
+
+struct QNode
+{
     int data;
-    QNode* next;
+    QNode *next;
     QNode(int d)
     {
         data = d;
         next = NULL;
     }
 };
- 
-struct Queue {
+
+struct Queue
+{
     QNode *front, *rear;
     Queue()
     {
         front = rear = NULL;
     }
- 
+
     void enQueue(int x)
     {
- 
+
         // Create a new LL node
-        QNode* temp = new QNode(x);
- 
+        QNode *temp = new QNode(x);
+
         // If queue is empty, then
         // new node is front and rear both
-        if (rear == NULL) {
+        if (rear == NULL)
+        {
             front = rear = temp;
             return;
         }
- 
+
         // Add the new node at
         // the end of queue and change rear
         rear->next = temp;
         rear = temp;
     }
- 
+
     // Function to remove
     // a key from given queue q
     void deQueue()
@@ -49,25 +52,23 @@ struct Queue {
         // If queue is empty, return NULL.
         if (front == NULL)
             return;
- 
+
         // Store previous front and
         // move front one node ahead
-        QNode* temp = front;
+        QNode *temp = front;
         front = front->next;
- 
+
         // If front becomes NULL, then
         // change rear also as NULL
         if (front == NULL)
             rear = NULL;
- 
+
         delete (temp);
     }
 };
- 
-// Driven Program
 int main()
 {
- 
+
     Queue q;
     q.enQueue(10);
     q.enQueue(20);
@@ -80,4 +81,7 @@ int main()
     cout << "Queue Front : " << (q.front)->data << endl;
     cout << "Queue Rear : " << (q.rear)->data;
 }
-// This code is contributed by rathbhupendra
+/*
+Queue Front : 40
+Queue Rear : 50
+ */
