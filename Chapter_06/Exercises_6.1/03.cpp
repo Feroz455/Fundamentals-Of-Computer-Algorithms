bc8ebfc1a66b36541ff3e0d2f6f39ed1a1bdb64f
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-05-30 11:44:25 +0600

    Add files via upload

---
+++ Chapter_06/Exercises_6.1/03.cpp
@@ -0,0 +1,82 @@
+/*
+Page 337
+03.
+Use one of the three traversal methods discussed in section 6.1 to
+obtain in algorithm Equiv(t,u) that determines whether the binary
+trees t and u are equivalent. Two binary trees t and u are equivalent
+if and only if they are structurally equivalent and if the data in the
+corresponding nodes of t and u are the same.
+*/
+
+// C++ program to see if two trees are identical
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+};
+
+/* Helper function that allocates a new node with the
+given data and NULL left and right pointers. */
+node *newNode(int data)
+{
+    node *Node = new node();
+    Node->data = data;
+    Node->left = NULL;
+    Node->right = NULL;
+
+    return (Node);
+}
+
+/* Given two trees, return true if they are
+structurally identical */
+int identicalTrees(node *a, node *b)
+{
+    /*1. both empty */
+    if (a == NULL && b == NULL)
+        return 1;
+
+    /* 2. both non-empty -> compare them */
+    if (a != NULL && b != NULL)
+    {
+        return (
+            a->data == b->data &&
+            identicalTrees(a->left, b->left) &&
+            identicalTrees(a->right, b->right));
+    }
+
+    /* 3. one empty, one not -> false */
+    return 0;
+}
+
+/* Driver code*/
+int main()
+{
+    node *root1 = newNode(1);
+    node *root2 = newNode(1);
+    root1->left = newNode(2);
+    root1->right = newNode(3);
+    root1->left->left = newNode(4);
+    root1->left->right = newNode(5);
+
+    root2->left = newNode(2);
+    root2->right = newNode(3);
+    root2->left->left = newNode(4);
+    root2->left->right = newNode(5);
+
+    if (identicalTrees(root1, root2))
+        cout << "Both tree are identical.";
+    else
+        cout << "Trees are not identical.";
+
+    return 0;
+}
+/*
+Both tree are identical.
+*/
