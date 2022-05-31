1cadea69b3d09d28739509c8e7705c722fd1b329
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-05-31 09:51:52 +0600

    Add files via upload

---
+++ Chapter_06/Algorithm/Algorithm_05.cpp
@@ -0,0 +1,119 @@
+/*
+Page 341
+Algorithm 6.5 Pseudocode for breath first search
+
+Algorithm BFS(v)
+//A breath first search of G is carried out beginning at vertex v,
+//For any node i, visited[i] = 1 if i has already been visited. The graph G and arry visited[]
+//are global; visited is initialize to zero
+{
+    u = v; // q is a queue of unexplored vertices
+    visited[v] = 1;
+    while(true)
+    {
+        for all vertices w adjacent from u do
+        {
+            if(visited[w] == 0)
+            {
+                add w to q; // w is unexplored
+                visited[w] = 1;
+            }
+        }
+        if q is empty then retrun; // No unexplored vertex Deleted the next element, u from q;
+        //Get first unexplored vertex
+    }
+}
+*/
+
+// Program to print BFS traversal from a given
+// source vertex. BFS(int s) traverses vertices
+// reachable from s.
+#include <bits/stdc++.h>
+using namespace std;
+
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+    int V; // No. of vertices
+
+    // Pointer to an array containing adjacency
+    // lists
+    vector<list<int>> adj;
+
+public:
+    Graph(int V); // Constructor
+
+    // function to add an edge to graph
+    void addEdge(int v, int w);
+
+    // prints BFS traversal from a given source s
+    void BFS(int s);
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj.resize(V);
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v’s list.
+}
+
+void Graph::BFS(int s)
+{
+    // Mark all the vertices as not visited
+    vector<bool> visited;
+    visited.resize(V, false);
+
+    // Create a queue for BFS
+    list<int> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+
+    while (!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued
+        // vertex s. If a adjacent has not been visited,
+        // then mark it visited and enqueue it
+        for (auto adjecent : adj[s])
+        {
+            if (!visited[adjecent])
+            {
+                visited[adjecent] = true;
+                queue.push_back(adjecent);
+            }
+        }
+    }
+}
+
+// Driver program to test methods of graph class
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
+
+    return 0;
+}
+/*
+2 0 3 1
+*/
