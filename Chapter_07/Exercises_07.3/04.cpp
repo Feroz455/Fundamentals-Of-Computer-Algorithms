cf711de25fe7220dadc2de742b423a58e473858a
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-06-07 13:10:28 +0600

    Add files via upload

---
+++ Chapter_07/Exercises_07.3/04.cpp
@@ -0,0 +1,52 @@
+/*
+Page 379
+04) Write a backtraking algorithm for the sum of subsets problem using the 
+state space tree corresponding to the variable tupple size formulation
+*/
+#include <iostream>
+using namespace std;
+#define N 6
+#define M 30
+int x[N];
+int w[N] = {5, 10, 12, 13, 15, 18};
+void SumOfSub(int s, int k, int r)
+// Find all subsets of w[1:n] that sum to m. The values of x[j],
+// a <= j < k; have already been determined . s = sumof(w[j]*x[j])
+// and r = sumof(w[j]). the w[j]'s are in nondecreasing order.
+// It is assumed that w[1] <= m  and sumof(w[i] > = m)
+{
+    cout << "s -> " << s << " k-> " << k + 1 << " r-> " << r << "\n";
+
+    // Generate left child. Notes s + w[k]<= m since B[k-1] is true
+    x[k] = 1;
+    if (s + w[k] == M)
+    {
+        // Subset found
+        for (int i = 0; i <= k; i++)
+        {
+            cout << x[i] << " ";
+        }
+        cout << "\n";
+    }
+    // there is no recursive call here as w[j] > 0, 1 <= j <= n
+    else if ((s + w[k] + w[k + 1]) <= M)
+    {
+        SumOfSub(s + w[k], k + 1, r - w[k]);
+    }
+    // Generate right child and evaluate Bk;
+    if ((s + r - w[k] >= M) && s + w[k + 1] <= M)
+    {
+        x[k] = 0;
+        SumOfSub(s, k + 1, r - w[k]);
+    }
+}
+int main()
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += w[i];
+    }
+    int s = 0, k = 0, r = sum;
+    SumOfSub(s, k, r);
+}
