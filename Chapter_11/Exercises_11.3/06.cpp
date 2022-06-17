b15fcac6b1afe5e6294d6591963e9e8ab532604e
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-06-17 13:59:27 +0600

    Add files via upload

---
+++ Chapter_11/Exercises_11.3/06.cpp
@@ -0,0 +1,14 @@
+/*
+Page 551
+6) 
+procedure fas2fvs(G = (v,E), G' = (V', E'))
+{
+    V' = E;
+    for each arc ei = (vi, vj) belongs E
+
+    if(there exists ej = (vj, vk) belong E)
+    E' = E' U {(e[i]), e[j]}
+    fi;
+    rof;
+}
+*/
