30770c6cea440df266b12be7dc559c9d194d5e83
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-06-18 22:31:47 +0600

    Add files via upload

---
+++ Chapter_12/Algorithm/Algorithm_12-01.cpp
@@ -0,0 +1,18 @@
+/*
+Page 580
+Algorithm 12.1 Approximate coloring 
+
+Algorithm AColor(V,E)
+//Determine an approximation to the minimum number of colors
+{
+    if V = 0 then
+    retrun 0;
+    else 
+    if (e = 0)
+    return 1;
+    else if G is bipart 
+    return 2;
+    else 
+    return 4;
+}
+*/
